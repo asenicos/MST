d1777e3a7c17ebcd0d327f2e261fad8bc6458cbf
asenicos <31521207+asenicos@users.noreply.github.com>
2024-11-01 12:08:48 +0300

    Update analysis code

--- stroop_analysis.py
+++ stroop_analysis.py
@@ -7,15 +7,15 @@ import os
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+import seaborn as sns
 from scipy.stats import ttest_ind
 
 
 #read Stroop test results
 exp='stroop.csv'
-path='.\\Data\\'
+path='d:\\my_scripts\\py\\stroopy-master\\Data\\'
 os.chdir(path)
 data=pd.read_csv(exp, sep=";")
-means = data.groupby(['congruent', 'Accuracy'])['RT'].mean()
 congr = np.where([data['congruent'] ==1, data['Accuracy']==1], data['RT'],np.nan)[0]
 ncongr = np.where([data['congruent'] ==0, data['Accuracy']==1], data['RT'],np.nan)[0]
 fig=np.vstack((ncongr, congr))
@@ -27,38 +27,24 @@ for i, d in enumerate(fig):
    plt.scatter(x, y)
 plt.show()
 
+print('Size of Stroop effect is', round((medians[0,1]-medians[1,1])*1000,3), '+/-', round((sems[0,1]-sems[1,1])*1000,3), 'ms')
+#Size of Stroop effect is 119.591 +/- 1.243 ms
+print('Analyzed ', len(data), ' trials, accuracy is ', round(len(data[data['Accuracy']==1])/len(data)*100,1), '%.')
+#Analyzed  144  trials, accuracy is  95.8 %.
+medians = data.groupby(['congruent', 'Accuracy'])['RT'].median()
+sems = data.groupby(['congruent', 'Accuracy'])['RT'].sem()
 t_statistic, p_value = ttest_ind(ncongr[np.isfinite(ncongr)], congr[np.isfinite(congr)])
 # Output the results
-print(f"t-statistic: {t_statistic}")
-print(f"P-value: {p_value}")
-# t-statistic: 3.76125479473406
-# P-value: 0.0004767207214359306
+print("t-statistic",round(t_statistic,3))
+print("p-value:", round(p_value,3))
+#t-statistic: 2.755
+#p-value: 0.007
 
-#for colors separately
-cred = np.vstack((congr, data['colour']=='red'))
-outr=np.where(cred[1], cred[0], np.nan)#only red
-cgreen = np.vstack((congr, data['colour']=='green'))
-outg=np.where(cgreen[1], cgreen[0], np.nan)#only green
-cblue = np.vstack((congr, data['colour']=='blue'))
-outb=np.where(cblue[1], cblue[0], np.nan)#only blue
-cyel = np.vstack((congr, data['colour']=='yellow'))
-outy=np.where(cyel[1], cyel[0], np.nan)#only green
+#by colors 
+colors =  ['green','magenta','black', 'orange','white','skyblue','yellow','red','cyan', 'brown','blue']#pd.unique(pd.Series(data.colour))
+#sns.catplot(data=data, kind='box', col='colour', x='congruent', y='RT', hue='colour', sharey=True, height=4)    
+sns.boxplot(x = data['congruent'], y = data['RT'], hue = data['colour'],palette = colors).legend_.remove()
 
-#ncong
-nred = np.vstack((ncongr, data['colour']=='red'))
-noutr=np.where(nred[1], nred[0], np.nan)#only red
-ngreen = np.vstack((ncongr, data['colour']=='green'))
-noutg=np.where(ngreen[1], ngreen[0], np.nan)#only green
-nblue = np.vstack((ncongr, data['colour']=='blue'))
-noutb=np.where(nblue[1], nblue[0], np.nan)#only blue
-nyel = np.vstack((ncongr, data['colour']=='yellow'))
-nouty=np.where(nyel[1], nyel[0], np.nan)#only green
-
-fig=np.vstack((outr, outg, outb, outy, noutr, noutg, noutb, nouty))
-fig=pd.DataFrame(fig).transpose()
-fig.boxplot()
-for i, d in enumerate(fig):
-   y = fig[d]
-   x = np.random.normal(i + 1, 0.04, len(y))
-   plt.scatter(x, y)
-plt.show()
+#by colors grouped
+sns.set_context("paper", font_scale=0.8)
+sns.boxplot(x = data['colour'], y = data['RT'], hue = data['congruent'], gap=0.2)
